c93830ffc2008f66480fe0f75298e56d87a9729d
Umut Önder <umutsonder@gmail.com>
2023-12-13 22:31:30 +0100

    Refactor getBaseUrl function to be inline in client-provider.tsx

--- src/app/_trpc/client-provider.tsx
+++ src/app/_trpc/client-provider.tsx
@@ -4,7 +4,15 @@ import { httpBatchLink, loggerLink } from "@trpc/client";
 import React, { useState } from "react";
 
 import { trpc } from "./client";
-import getBaseUrl from "@/utils/getBaseUrl";
+
+const getBaseUrl = () => {
+	if (typeof window !== "undefined") return ""; // browser should use relative url
+
+	if (process.env.NODE_ENV === "development")
+		return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+};
 
 export default function TRPCProvider({
 	children,
