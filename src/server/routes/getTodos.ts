3d02088d799265a3100b9a3b1f1aa6cf992ce167
Umut Önder <umutsonder@gmail.com>
2023-12-13 22:45:08 +0100

    Update import statement in getTodos.ts

--- src/server/routes/getTodos.ts
+++ src/server/routes/getTodos.ts
@@ -1,8 +1,8 @@
-import { todos } from "../db/schema";
+import * as table from "../db/schema";
 import { publicProcedure, router } from "../trpc";
 
 export const TodosRouter = router({
 	get: publicProcedure.query(async ({ ctx }) => {
-		return await ctx.db.select().from(todos);
+		return await ctx.db.select().from(table.todos);
 	}),
 });
