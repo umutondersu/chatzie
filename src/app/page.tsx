ad1c9165ccb27525934d58102317232011eae8df
Umut Önder <umutsonder@gmail.com>
2023-12-17 23:59:45 +0100

    styled the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,26 @@ export default async function Home() {
 	const todos = await api.Todos.get();
 	return (
 		<main className="max-w-3xl mx-auto mt-5">
-			<h1 className="text-3xl font-bold">Todo App</h1>
-			<TodoList initialTodos={todos} />
-			<UserPanel />
+			<h1 className="text-2xl">
+				<span className="font-bold text-3xl">Chatzie</span> a Todo App
+			</h1>
+			<div className="border border-double p-2">
+				<TodoList initialTodos={todos} />
+				<UserPanel />
+			</div>
+			<article className="relative top-10">
+				<div className="underline text-2xl mb-2">Technologies used</div>
+				<ul className="[&>*]:text-xl">
+					<li>- Next.js</li>
+					<li>- TRPC </li>
+					<li>- React Query for Data Management</li>
+					<li>- Clerk for Authentication</li>
+					<li>- Turso for DB on the Edge (formerly PlanetScale)</li>
+					<li>- Drizzle ORM</li>
+					<li>- Typescript</li>
+					<li>- TailwindCSS for styling</li>
+				</ul>
+			</article>
 		</main>
 	);
 }
