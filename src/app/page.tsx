5d01a78849f1bd2e03e9a0aa42914b50ef495359
Umut Önder <umutsonder@gmail.com>
2023-12-17 19:09:09 +0100

    Added a "Todo App" header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default async function Home() {
 	const todos = await api.Todos.get();
 	return (
 		<main className="max-w-3xl mx-auto mt-5">
+			<h1 className="text-3xl font-bold">Todo App</h1>
 			<TodoList initialTodos={todos} />
 			<UserPanel />
 		</main>
